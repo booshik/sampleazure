563260f2b70bce078bafd18a95cbb8eae4aed609
booshika.a <booshika.a@saint-gobain.com>
2023-12-02 12:48:56 +0530

    name added

--- sampleazure/Controllers/WeatherForecastController.cs
+++ sampleazure/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace sampleazure.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Boosh", "Sashi", "Sownd", "Shanu"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
